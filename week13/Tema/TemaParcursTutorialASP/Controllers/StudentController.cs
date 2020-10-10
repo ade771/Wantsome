a6949dfd9f18270acc279f670cf6a2def4225f02
ade771 <mantaluta82@gmail.com>
2020-10-10 16:54:24 +0300

    Tema ASP

---
+++ week13/Tema/TemaParcursTutorialASP/Controllers/StudentController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TemaParcursTutorialASP.Models;
+
+namespace TemaParcursTutorialASP.Controllers
+{
+    public class StudentController : Controller
+    {
+        Student student;
+        public StudentController()
+        {
+            student = new Student();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            List<Student> students = student.GetAll();
+            
+            return View(students);
+        }
+
+        [HttpGet]
+        public ActionResult View(int id)
+        {
+            var studentid = student.GetById(id);
+
+            return View(studentid);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Add(student);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            if (ModelState.IsValid)
+            {
+                student.Delete(student);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+        
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var students = student.GetById(id);
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult EditStudent(Student student)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Student student)
+        {
+            var existingStudent = student.GetById(id);
+
+            if (ModelState.IsValid)
+            {
+                existingStudent.StudentName = student.StudentName;
+                existingStudent.Age = student.Age;
+
+                return RedirectToAction("Index");
+            }
+
+            return View(existingStudent);
+        }
+    }
+}
---
+++ week13/Tema/TemaParcursTutorialASP/Models/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TemaParcursTutorialASP.Models
+{
+    public class Student
+    {
+
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+        static List<Student> studentList = new List<Student>{
+                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                new Student() { StudentId = 6, StudentName = "Chris" , Age = 17 } ,
+                new Student() { StudentId = 7, StudentName = "Rob" , Age = 19 }
+            };
+
+        public List<Student> GetAll()
+        {
+            return studentList;
+        }
+
+        public Student GetById(int id)
+        {
+            return studentList.FirstOrDefault(x => x.StudentId == id);
+        }
+
+        public void Add(Student student)
+        {
+            student.StudentId = GetMaxId();
+
+            studentList.Add(student);
+        }
+
+
+        public void Delete(Student student)
+        {
+            student.StudentId = GetMaxId();
+
+            studentList.Remove(student);
+        }
+
+        private int GetMaxId()
+        {
+            return studentList.Count + 1;
+        }
+    }
+}
